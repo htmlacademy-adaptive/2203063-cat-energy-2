2d821bdee1a5d4e186e9ce3327fdc8890f930db3
ViktoriyaKnoroz <viktoriya.knoroz@mail.ru>
2025-04-24 12:38:09 +0300

    Дополняет стилизацию главной страницы

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,9 @@
-/* в этот файл добавляет скрипты*/
+const nav = document.querySelector('.nav');
+const button = document.querySelector('.nav__toggle');
+
+nav.classList.add('nav--closed');
+
+button.addEventListener('click', () => {
+    nav.classList.toggle('nav--closed');
+    nav.classList.toggle('nav--opened');
+})
